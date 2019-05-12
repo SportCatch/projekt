99d6a47e81350a3c4eb46b10fb4458f06ed92073
jacenty2 <jacek.ciszkowski@wp.pl>
2019-05-12 22:33:43 +0200

    macionowe

--- event/templatetags/filter.py
+++ event/templatetags/filter.py
@@ -1,8 +1,10 @@
 from django import template
-
+from datetime import datetime, timedelta
 from django.template.defaulttags import register
 from ..models import *
+from django.utils import timezone
 from account.models import Profile
+from datetime import date
 register = template.Library()
 
 
@@ -15,3 +17,29 @@ def is_friend(arg,user_id):
                         return False
 
         return True
+
+
+@register.filter
+def is_ok(wyrzenia,user):
+        
+        wydarzenia=wydarzenie.objects.get(pk=wyrzenia)
+        Ocena = Ocena_wydarzenia.objects.filter(wydarzenie=wydarzenia,user=user)
+        Data=wydarzenia.data_rozpoczecia
+        today=date.today()
+        wynik=today-Data
+
+        if wynik.days >7:
+                return True
+
+        if Data>today:
+                return True
+        
+        if not Ocena:
+                 return False
+       
+        
+
+        return True
+        
+
+ 
